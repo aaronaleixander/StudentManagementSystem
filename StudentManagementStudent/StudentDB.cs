26819bffaee9d5b53e3712cff47d3070739067e9
Aaron Utterback <aar0nu@outlook.com>
2019-05-23 10:38:45 -0700

    Created Database

--- StudentManagementStudent/StudentDB.cs
+++ StudentManagementStudent/StudentDB.cs
@@ -15,7 +15,23 @@ namespace StudentManagementStudent
 
         public static void Add(Student s)
         {
-
+            throw new NotImplementedException();
         }
+        public static void Update(Student s)
+        {
+            throw new NotImplementedException();
+        }
+        public static void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        //public static double DoubleNum(int a)
+        //{
+        //    return a * 2;
+        //}
+
+        // C# Expression-bodied method using Lamda expressions. Syntactic sugar feats.
+        public static double DoubleNum(int a) => a * 2;
     }
 }
